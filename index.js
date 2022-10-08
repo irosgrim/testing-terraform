bb037812dd826398c861032569c2a93cf45e9fce
ioan rosgrim <ioan.rosgrim@ingka.ikea.com>
2022-10-08 17:59:55 +0200

    port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 const cors = require("cors");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.get("/", (req, res) => {
