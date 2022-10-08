0374a2dc55a9c355ecae29c63d9ce664d0857f81
ioan rosgrim <ioan.rosgrim@ingka.ikea.com>
2022-10-08 17:36:00 +0200

    initial

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+const PORT = 3000;
+
+app.use(cors());
+app.get("/", (req, res) => {
+    res.send([
+        {
+            id: 1,
+            name: "Hello world",
+            done: false
+        },
+        {
+            id: 2,
+            name: "Buy milk",
+            done: false
+        },
+        {
+            id: 3,
+            name: "Go out",
+            done: true
+        }
+    ])
+})
+
+app.listen(PORT, () => console.log("Server is running on port ", PORT))
